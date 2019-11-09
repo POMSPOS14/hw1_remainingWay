fee9fc5551bb60d27d4c9b77fc46aab163fd4dda
Ромик <pomah14021997@mail.ru>
2019-11-09 14:08:57 +0300

    Created a method for calculating the path

---
+++ src/main/java/ru/rosbank/javashcool/service/remainingWayService.java
@@ -0,0 +1,14 @@
+package ru.rosbank.javashcool.service;
+
+public class remainingWayService {
+
+    public int calculate(int consumption, int fuel) {
+        int consumptionInML = consumption * 1000;
+        int fuelInML = fuel * 1000;
+
+        int approximateWay = fuelInML / (consumptionInML / 100);
+
+        return approximateWay;
+    }
+
+}
