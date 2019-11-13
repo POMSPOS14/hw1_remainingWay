2053e51d9a57435fc7f1857a7a6abb5b3e920e09
POMSPOS14 <pomah14021997@mail.ru>
2019-11-13 17:22:44 +0300

    fixes #2

--- src/main/java/ru/rosbank/javashcool/service/RemainingWayService.java
+++ src/main/java/ru/rosbank/javashcool/service/RemainingWayService.java
@@ -5,8 +5,9 @@ public class RemainingWayService {
     public int calculate(int consumption, int fuel) {
         int consumptionInML = consumption * 1000;
         int fuelInML = fuel * 1000;
+        int consumptionForOneKm = consumptionInML / 100;
 
-        int approximateWay = fuelInML / (consumptionInML / 100);
+        int approximateWay = fuelInML / (consumptionForOneKm);
 
         return approximateWay;
     }
